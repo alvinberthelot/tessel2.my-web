714e849e040255474fe80148d77f2f7a7ff991a7
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2016-05-20 22:13:13 +0200

    🌱  Hello world with simple HTTP server

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Import the interface to Tessel hardware
 var tessel = require('tessel');
+// Load the http module to create an http server.
+var http = require('http');
 
-// Turn one of the LEDs on to start.
-tessel.led[2].on();
+// Configure our HTTP server to respond with "Hello from Tessel!" to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello from Tessel!\n');
+});
 
-// Blink!
-setInterval(function () {
-  tessel.led[2].toggle();
-  tessel.led[3].toggle();
-}, 100);
+// Listen on port 8080, IP defaults to 192.168.1.101. Also accessible through [tessel-name].local
+server.listen(8080);
 
-console.log("I'm blinking! (Press CTRL + C to stop)");
+// Put a friendly message in the terminal
+console.log('Server running at http://192.168.1.101:8080/');
