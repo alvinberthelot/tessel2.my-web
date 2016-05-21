1a13cac9134c74be4c3119c3fa7e0d8001cc88a7
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2016-05-21 17:22:03 +0200

    🌱  (Deploy static files) : Add CSS file with index.html, found in a static directory

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ var express = require('express');
 
 var app = express();
 
+app.use('/static', express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
   res.status(200).sendFile('index.html', { root: __dirname });
 });
