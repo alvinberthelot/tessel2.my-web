0b22a9417e7040146292d40c4ba316cb8b3ba21e
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2016-05-22 11:28:47 +0200

    🌱  Add first API with test

--- src/server.js
+++ src/server.js
@@ -12,4 +12,15 @@ app.get('/', function(req, res) {
   res.status(200).sendFile('index.html', { root: __dirname });
 });
 
+app.get('/api/microcontroller', function(req, res) {
+  res.header('Content-Type', 'application/json');
+  res.charset = 'utf-8';
+  res.status(200).json({
+    message: 'Give info about microcontroller used',
+    data: {
+      name: 'gaius'
+    }
+  });
+});
+
 module.exports = app;
